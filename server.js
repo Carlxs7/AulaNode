82caf1452ac156d4567dcf631c788596d2f17f15
CARLOS EDUARDO LIRA DE ASSIS <carlosassis0842@digitalcollege.internal>
2026-03-28 11:46:51 -0300

    Meu primeiro commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Requisição recebida!");
+});
+
+app.get('/home', (req, res) => {
+    res.send("Home page");
+});
+
+app.get('/me', (req, res) => {
+    console.log(req.query);
+    res.send(`Meu nome é ${req.query.nome}, Moro em ${req.query.cidade}`);
+})
+
+app.listen(1234, () => {
+    console.log("Servidor inicado na porta  1234");
+});
