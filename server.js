81e3598c0940304fd44a30c7cefa9b8895223546
CARLOS EDUARDO LIRA DE ASSIS <carlosassis0842@digitalcollege.internal>
2026-04-11 11:51:01 -0300

    Iniciando post

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Requisição recebida!");
 });
@@ -13,7 +15,33 @@ app.get('/home', (req, res) => {
 app.get('/me', (req, res) => {
     console.log(req.query);
     res.send(`Meu nome é ${req.query.nome}$ git , Moro em ${req.query.cidade}`);
-})
+});
+
+app.get('/produtos{/*categoria}{/*produto}', (req, res) => {
+    const {categoria, produto} = req.params;
+
+    if(categoria && produto) {
+       return res.send(`Pagina do produto ${produto}`);
+    }
+
+    if (categoria && !produto) {
+        return res.send(`Todos os produtos da categoria ${categoria}`)
+    }
+
+    return res.send(`Todos os produtos`);
+
+});
+
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+
+    if (email === 'email@mail.com' && password === 'senha123') {
+        return res.send("Login bem-sucedido!");
+    }
+
+    return res.send('Login falhou. Email ou senha incorretos.');
+
+});
 
 app.listen(1234, () => {
     console.log("Servidor inicado na porta  1234");
