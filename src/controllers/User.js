fefffb6eb7e06700b35754043dac38fd255a732d
CARLOS EDUARDO LIRA DE ASSIS <carlosassis0842@digitalcollege.internal>
2026-04-25 11:38:32 -0300

    criado novas rotas

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,25 +1,58 @@
-
-const FakeUsers = [
+const fakeUsers = [
     {
+        id:1,
         name: 'Cleitin',
         email: 'cleitin@mail.com'
-    },
+    }
 ];
 
-function ListUser(req, res) {
-    res.json(fakeUsers);
+async function ListUser(req, res) {
+    res.send(fakeUsers);
+}
+
+async function FindUser(req, res) {
+    const {id} = req.params;
+    const user = fakeUsers.find(user => Number(user.id) === Number(id));
+
+    if(!user) {
+        return res.status(404).json({error: "Not found"});
+    }
+
+    return res.json(user);
 }
 
 function CreateUser(req, res) {
     const {name, email} = req.body;
-    fakeUsers.push({name, email});
+    const id = fakeUsers.length + 1;
+    fakeUsers.push({id, name, email});
     res.json({
-        message: "Usuario criado com sucesso",
+        message: "Usuario cadastrado com sucesso!"
     });
 }
 
-    
+function UpdateUser(req, res) {
+    const {id} = req.params;
+    const {name, email} = req.body;
+
+    fakeUsers.forEach(user => {
+        if(Number(user.id) === Number(id)) {
+            user.name = name || user.name;
+            user.email = email || user.email;
+        }
+    });
+
+    res.send({
+        message: "Usuario atualizado com sucesso!"
+    });
+}
+
+// let usuarioEncontrado = fakeUsers.find(user => Number(user.id) === Number(id));
+//  usuarioEncontrado.name = name || usuarioEncontrado.name
+//  usuarioEncontrado.email = email || usuarioEncontrado.email
+
 module.exports = {
+    ListUser,
+    FindUser,
     CreateUser,
-    ListUser
+    UpdateUser
 }
